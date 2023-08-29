c7b158af45833b6c664f677f9ea99266dfbe6dbb
SzatanEmil <mateuszmisiejuk21@gmail.com>
2023-08-29 19:42:10 +0200

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -19,3 +19,7 @@
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
